f02ca6f9d86487692885b921996bb0d8349d938c
ThuanVan060504 <nhockisolo@gmail.com>
2025-10-08 15:14:05 +0700

    update code mới

--- cloud.js
+++ cloud.js
@@ -1,45 +1,41 @@
-var typed = new Typed('#element', {
-    strings: ['<i>FrontEnd Developer</i>', ' <i>Application Developer</i>', ' <i>Competitive Programmer</i>'],
-    typeSpeed:100,
-    backSpeed:100,
-    backDelay:1000,
-    loop:true
-});
+document.addEventListener('DOMContentLoaded', () => {
+  var typed = new Typed('#element', {
+    strings: [
+      '<i>Web Developer</i>',
+      '<i>Application Developer</i>',
+      '<i>Competitive Programmer</i>'
+    ],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+  });
 
-ddocument.addEventListener('DOMContentLoaded', () => {
-    var typed = new Typed('#element', {
-        strings: ['<i>FrontEnd Developer</i>', ' <i>Application Developer</i>', ' <i>Competitive Programmer</i>'],
-        typeSpeed: 100,
-        backSpeed: 100,
-        backDelay: 1000,
-        loop: true
-    });
+  let menuIcon = document.querySelector('#menu-icon');
+  let sidePanel = document.querySelector('#navbar');
+  let navLinks = document.querySelectorAll('.header1 nav a');
+  let sections = document.querySelectorAll('section');
 
-    let menuIcon = document.querySelector('#menu-icon');
-    let sidePanel = document.querySelector('#navbar');
-    let navLinks = document.querySelectorAll('.header1 nav a');
-    let sections = document.querySelectorAll('section');
+  menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    sidePanel.classList.toggle('active');
+  };
 
-    menuIcon.onclick = () => {
-        menuIcon.classList.toggle('bx-x');
-        sidePanel.classList.toggle('active');
-    };
-
-    window.onscroll = () => {
-        let top = window.scrollY;
-        sections.forEach(sec => {
-            let offset = sec.offsetTop - 150;
-            let height = sec.offsetHeight;
-            let id = sec.getAttribute('id');
-            if (top >= offset && top < offset + height) {
-                navLinks.forEach(link => {
-                    link.classList.remove('active');
-                    let activeLink = document.querySelector('.header1 nav a[href*=' + id + ']');
-                    if (activeLink) {
-                        activeLink.classList.add('active');
-                    }
-                });
-            }
+  window.onscroll = () => {
+    let top = window.scrollY;
+    sections.forEach(sec => {
+      let offset = sec.offsetTop - 150;
+      let height = sec.offsetHeight;
+      let id = sec.getAttribute('id');
+      if (top >= offset && top < offset + height) {
+        navLinks.forEach(link => {
+          link.classList.remove('active');
+          let activeLink = document.querySelector(`.header1 nav a[href*="${id}"]`);
+          if (activeLink) {
+            activeLink.classList.add('active');
+          }
         });
-    };
+      }
+    });
+  };
 });
